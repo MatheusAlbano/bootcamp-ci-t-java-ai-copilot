8e79bacc2eff90ee4ddb4a09db323aa9aaca79e5
MatheusAlbano <albanoportomatheus@gmail.com>
2026-09-20 15:38:29 -0300

    feat: add example using for loop structure

--- 4-estruturas-de-controles-java/for/src/Main.java
+++ 4-estruturas-de-controles-java/for/src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        for (var i = 0; i < args.length; i++){
+            System.out.println((i+1) + " - " + args[i]);
+        }
     }
 }
