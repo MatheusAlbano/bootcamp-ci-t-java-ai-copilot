b804417179e1d0df81aa6cd994386c064b129136
MatheusAlbano <albanoportomatheus@gmail.com>
2026-09-21 22:04:47 -0300

    feat: learning how to use records

---
+++ 5-java-e-a-arte-da-abstracao-com-classes-e-encapsulamento/using-records/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        var person = new Person("Matheus");
+        System.out.println(person.getInfo());
+        System.out.println(person.name());
+    }
+}
